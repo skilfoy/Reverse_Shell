04013676eace65438f1d8d43341d7e5f8daf5041
skilfoy <20802814+skilfoy@users.noreply.github.com>
2021-07-10 03:33:21 -0500

    Update reverse_shell.py

--- reverse_shell.py
+++ reverse_shell.py
@@ -3,6 +3,15 @@ import socket
 
 sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 sock.connect(("127.0.0.1",54321))
-
 print("Connection Established to Server")
+
+while True:
+    message = sock.recv(1024)
+    print(message)
+    if message == "q":
+        break
+    else:
+        message_back = raw_input("Type message to send to server: ")
+        sock.send(message_back)
+        
 sock.close()
